a9608e70c51b6c947749c2c2790ca6f49de034a1
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-01 17:09:55 -0500

    project setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
