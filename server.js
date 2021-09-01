67f0d72a3d622ff7905eae62c175f23d832c29c2
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-01 17:30:56 -0500

    modified server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const express = require('express');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mo-all-go', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
