dffb5ed93b2f31f058a5ec845a91a5e541eb3bef
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-09-04 11:45:00 -0500

    linked thought routes in index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-// add prefix of users routes
+// add prefix of user routes
 router.use('/user', userRoutes);
+// add prefix of thought routes
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
